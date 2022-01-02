45887cd91939890f83bb6709501f5f22dd9790ed
kpjha786 <kpjha786@gmail.com>
2022-01-02 21:44:04 +0530

    heading

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Game from './components/game';
 class App extends React.Component {
   render(){
     return(
+      <div>
+      <h1>TicTacToe</h1>
       <Game/>
+      </div>
     )
   }
 }
